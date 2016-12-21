b9a9e28a07aa428f30ed696e1e251186e6f095ea
Aliaksandr Stsiarzhanau <sterzhanoff@gmail.com>
2016-12-21 16:13:21 +0300

    Add todos reducer and tests

--- src/reducers/reducers.jsx
+++ src/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+import moment from 'moment';
+import uuid from 'uuid';
+
 export const searchTextReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
@@ -11,6 +14,41 @@ export const showCompletedReducer = (state = false, action) => {
   switch (action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
+    default:
+      return state;
+  }
+};
+
+var a;
+
+export const todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          text: action.text,
+          id: uuid(),
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined,
+        },
+      ];
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          // todo.completed = !todo.completed;
+          // todo.completedAt = todo.completed ? moment().unix() : undefined;
+          const nextCompleted = !todo.completed;
+
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined,
+          };
+        }
+        return todo;
+      });
     default:
       return state;
   }
--- src/tests/reducers/reducers.test.jsx
+++ src/tests/reducers/reducers.test.jsx
@@ -26,5 +26,37 @@ describe('Reducers', () => {
     });
   });
 
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      const action = {
+        type: 'ADD_TODO',
+        text: 'New Todo'
+      };
 
+      const result = reducers.todosReducer(df([]), df(action));
+
+      expect(result.length).toBe(1);
+      expect(result[0].text).toBe(action.text);
+    });
+
+    it('should toggle todo completed status', () => {
+      const action = {
+        type: 'TOGGLE_TODO',
+        id: '1'
+      };
+
+      const todos = [{
+        text: 'Todo to toggle',
+        id: '1',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125,
+      }];
+
+      const result = reducers.todosReducer(df(todos), df(action));
+
+      expect(result[0].completed).toBe(false);
+      expect(result[0].completedAt).toBe(undefined);
+    });
+  });
 });
